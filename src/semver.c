98450dca30263407ea4c0a26f941d8d56689712f
Xavier Caron <xcaron@gmail.com>
2016-10-17 14:01:06 +0200

    replaced strcasecmp invocation with proper section 11 implementation

--- src/semver.c
+++ src/semver.c
@@ -373,6 +373,70 @@ char* strip_meta(const char *str)
     return copy;
 }
 
+// http://semver.org/#spec-item-11:
+// Precedence for two pre-release versions with the same major, minor, and patch version MUST be determined
+// by comparing each dot separated identifier from left to right until a difference is found as follows:
+// identifiers consisting of only digits are compared numerically and identifiers with letters or hyphens
+// are compared lexically in ASCII sort order. Numeric identifiers always have lower precedence than
+// non-numeric identifiers. A larger set of pre-release fields has a higher precedence than a smaller set,
+// if all of the preceding identifiers are equal. Example:
+// 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0.
+
+#define TAIL_CMP_LT -1
+#define TAIL_CMP_EQ  0
+#define TAIL_CMP_GT +1
+#define TAIL_CMP_KO  9
+
+int tail_cmp ( char *lhs, char *rhs ) {
+  if ( !strcasecmp ( lhs, rhs ) ) return TAIL_CMP_EQ;
+
+  char *dot = ".";
+  char *l_last, *r_last;
+
+  char *l_token = strtok_r ( lhs, dot, &l_last );
+  char *r_token = strtok_r ( rhs, dot, &r_last );
+
+  if (  l_token && !r_token ) return TAIL_CMP_LT;
+  if ( !l_token &&  r_token ) return TAIL_CMP_GT;
+
+  while ( l_token || r_token ) {
+    if ( l_token && r_token ) {
+      int l_numeric = isdigit ( l_token[0] );
+      int r_numeric = isdigit ( r_token[0] );
+
+      if ( l_numeric && r_numeric ) {
+        int l_int = atoi ( l_token );
+        int r_int = atoi ( r_token );
+
+        if ( l_int < r_int ) return TAIL_CMP_LT;
+        if ( l_int > r_int ) return TAIL_CMP_GT;
+      }
+      else if ( l_numeric ) {
+        return TAIL_CMP_LT;
+      }
+      else if ( r_numeric ) {
+        return TAIL_CMP_GT;
+      }
+      else {
+        int cmp = strcasecmp ( l_token, r_token );
+
+        if ( cmp ) return cmp > 0 ? TAIL_CMP_GT : TAIL_CMP_LT;
+      }
+    }
+    else if ( l_token ) {
+      return TAIL_CMP_GT;
+    }
+    else if ( r_token ) {
+      return TAIL_CMP_LT;
+    }
+
+    l_token = strtok_r ( NULL, dot, &l_last );
+    r_token = strtok_r ( NULL, dot, &r_last );
+  }
+
+  return TAIL_CMP_KO;
+}
+
 int prerelcmp(const char* a, const char* b)
 {
     int res;
@@ -392,7 +456,7 @@ int prerelcmp(const char* a, const char* b)
     if (*ac != '\0' && *bc == '\0') {
         return -1;
     }
-    res = strcasecmp(ac, bc);
+    res = tail_cmp(ac, bc);
     pfree(ac);
     pfree(bc);
     return res;
